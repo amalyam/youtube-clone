703490a6ac6bd3e00af6f1ef5d9d746887399ee6
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-03 01:18:08 +0000

    remove root get request, add post request for process-video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -2,12 +2,36 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  // Get path of input video file from req body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    const missing =
+      !inputFilePath && !outputFilePath
+        ? "input and output file paths"
+        : !inputFilePath
+        ? "input file path"
+        : "output file path";
+    res.status(400).send(`Bad Request: Missing ${missing}.`);
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Video processing successfully completed.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Video processing service listening at port ${port}`);
-})
+  console.log(`Video processing service listening at port ${port}`);
+});
