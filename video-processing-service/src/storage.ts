42add03b071945aca3dc476bdfb26255265b5935
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-08 16:02:38 +0000

    create convertVideo() using methods previously in index.ts

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -10,3 +10,23 @@ const processedVideoBucketName = "video-service-processed-files";
 const localRawVideoPath = "./raw-video";
 const localProcessedVideoPath = "./processed-video";
 
+/**
+ * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}.
+ * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}.
+ * @returns a promise that resolves whe the video has been converted.
+ */
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+  return new Promise<void>((resolve, reject) => {
+    ffmpeg(`{localRawVideoPath}/${rawVideoName}`)
+      .outputOptions("-vf", "scale=-1:360")
+      .on("end", () => {
+        console.log("Video processing successfully completed.");
+        resolve();
+      })
+      .on("error", (err) => {
+        console.log(`An error occurred: ${err.message}`);
+        reject(err);
+      })
+      .save(`${localProcessedVideoPath}/${processedVideoName}`);
+  });
+}
