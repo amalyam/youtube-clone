d23c00cb8a4d8144156e872a6abefde31b12cb3d
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-12 01:51:45 +0000

    basic firebase config

---
+++ yt-web-client/yt-clone/app/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJVGpuu8GwgbgFOosl4t9xvn6axm6KFOw",
+  authDomain: "yt-clone-2618d.firebaseapp.com",
+  projectId: "yt-clone-2618d",
+  appId: "1:7264140205:web:5e40fbea4f73afd8dabf24",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
