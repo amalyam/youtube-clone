d7cc5a0ab701c4054c4de518e0ed365691b60f94
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-14 19:17:19 +0000

    configure firebase with env keys

---
+++ yt-web-client/yt-clone/app/firebase/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
