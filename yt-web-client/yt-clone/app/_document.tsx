4ecc02bd395ed67b24535f69af86fc2a0551861c
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-15 03:17:47 +0000

    create file to extend the default document structure of Next.js, add Roboto font link

---
+++ yt-web-client/yt-clone/app/_document.tsx
@@ -0,0 +1,24 @@
+/**
+ * This file allows the developer to extend the default document structure of Next.js
+ * in order to modify the <html>, <head>, and <body> tags
+ */
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
