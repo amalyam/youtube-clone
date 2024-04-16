13e05d2609d23052685e735a73db9c02703eb783
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-16 22:07:02 +0000

    use useState and useEffect to track user state

--- yt-web-client/yt-clone/app/components/navbar.tsx
+++ yt-web-client/yt-clone/app/components/navbar.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+import { unsubscribe } from "diagnostics_channel";
 
 export default function Navbar() {
+  // initialize user state
+  // TODO: refactor useState and useEffect into sign-in.tsx so navbar can be rendered on the server
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // cleanup subscription on unmount
+    return () => unsubscribe();
+  });
+
   return (
     <nav>
       <div className="flex justify-between items-center p-4">
@@ -16,6 +36,7 @@ export default function Navbar() {
           </span>
         </Link>
       </div>
+      <SignIn user={user} />
     </nav>
   );
 }
