389ec13091fe18ebe7730664c6c6d6f685925ac9
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-18 00:42:00 +0000

    add getVideosFunction and Video interface (wth refactor TODO)

--- yt-web-client/yt-clone/app/firebase/functions.ts
+++ yt-web-client/yt-clone/app/firebase/functions.ts
@@ -1,8 +1,22 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
-
-const functions = getFunctions();
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase";
 
 const generateUploadUrl = httpsCallable(functions, "generateUploadUrl");
+const getVideosFunction = httpsCallable(functions, "getVideos");
+
+// represents a video document in firestore collection
+// this interface also used in yt-api-service/functions/src/index.ts
+// and video-processing-service/src/firestore.ts
+// TODO refactor by creating video-api-service, implement REST API endpoints, make requests to the service (using axios?)
+export interface Video {
+  id?: string; // make required?
+  uid?: string; // make required?
+  filename?: string;
+  status?: "processing" | "processed";
+  title?: string;
+  description?: string;
+  // add date?
+}
 
 export async function uploadVideo(file: File) {
   const response: any = await generateUploadUrl({
