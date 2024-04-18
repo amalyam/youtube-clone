6b1c908ef30f1da4d00c5192eb972a5d10fbe061
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-18 00:53:49 +0000

    add error handling

--- video-processing-service/src/firestore.ts
+++ video-processing-service/src/firestore.ts
@@ -26,26 +26,40 @@ export interface Video {
 
 // retrieve and return video doc from firestore as Video object
 async function getVideo(videoId: string) {
-  const snapshot = await firestore
-    .collection(videoCollectionId)
-    .doc(videoId)
-    .get();
-  return (snapshot.data() as Video) ?? {};
+  try {
+    const snapshot = await firestore
+      .collection(videoCollectionId)
+      .doc(videoId)
+      .get();
+    return (snapshot.data() as Video) ?? {};
+  } catch (error) {
+    console.error("Failed to get video: ", error);
+    return null;
+  }
 }
 
 // sets data of corresponding video doc in firestore collection to Video obj or creates new doc
 export function setVideo(videoId: string, video: Video) {
-  return (
-    firestore
-      .collection(videoCollectionId)
-      .doc(videoId)
-      // if doc already exists, merge new data with existing
-      .set(video, { merge: true })
-  );
+  try {
+    return (
+      firestore
+        .collection(videoCollectionId)
+        .doc(videoId)
+        // if doc already exists, merge new data with existing
+        .set(video, { merge: true })
+    );
+  } catch (error) {
+    console.error("Failed to set video: ", error);
+  }
 }
 
 // retrieves corresponding video doc - if status field undefined, video is new
 export async function isVideoNew(videoId: string) {
-  const video = await getVideo(videoId);
-  return video?.status === undefined;
+  try {
+    const video = await getVideo(videoId);
+    return video?.status === undefined;
+  } catch (error) {
+    console.error("Failed to check if video is new: ", error);
+    return null;
+  }
 }
