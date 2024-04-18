3271d7dc3e824a0533796375c555e7e7b2994cf0
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-18 00:45:56 +0000

    use useRouter to get filename from URL and render video

--- yt-web-client/yt-clone/app/watch/page.tsx
+++ yt-web-client/yt-clone/app/watch/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useRouter } from "next/router";
+
 export default function Watch() {
+  const router = useRouter();
+  const videoPrefix =
+    "https://storage.googleapis.com/video-viewer-processed-files/";
+  const videoSrc = router.query.v;
+
   return (
     <div>
       <h1>Watch Page</h1>
+      {<video controls src={videoPrefix + videoSrc} />}
     </div>
   );
 }
