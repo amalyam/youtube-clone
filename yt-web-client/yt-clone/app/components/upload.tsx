1b47b8903ec1f2a5df38e2b05d26a80a77baf9a5
Amalya Megerman <115735046+amalyam@users.noreply.github.com>
2024-04-19 15:29:56 +0000

    disable input while uploading and change text to "Uploading..." until complete

--- yt-web-client/yt-clone/app/components/upload.tsx
+++ yt-web-client/yt-clone/app/components/upload.tsx
@@ -21,7 +21,7 @@ export default function Upload() {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.item(0);
     if (file) {
-      // wrapper for error handling
+      // TODO add additional error handling here (max file size, valid file types, etc)
       handleUpload(file);
     }
   };
@@ -34,12 +34,15 @@ export default function Upload() {
         accept="video/*"
         onChange={handleFileChange}
         className="sr-only"
+        disabled={uploading}
       />
       <label
         htmlFor="upload"
-        className="inline-block border border-gray-400 text-blue-600 px-5 py-2 rounded-full font-roboto text-sm font-medium cursor-pointer hover:bg-blue-200 hover:border-transparent"
+        className={`inline-block border border-gray-400 text-blue-600 px-5 py-2 rounded-full font-roboto text-sm font-medium ${
+          uploading ? "cursor-not-allowed" : "cursor-pointer"
+        } hover:bg-blue-200 hover:border-transparent`}
       >
-        Upload
+        {uploading ? "Uploading..." : "Upload"}
       </label>
     </Fragment>
   );
